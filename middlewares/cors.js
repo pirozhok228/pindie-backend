e4142faa1a97a406e2aeda2f11e5f61683b02d1e
pirozhok228 <vadimpirogov505@gmail.com>
2024-05-25 16:00:13 +0300

    Исправление cors

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,7 +2,7 @@ const allowedCors = [
     'https://practicum.yandex.ru',
     'https://students-projects.ru',
     'localhost:3000',
-    'localhost:3001'
+    'http://localhost:3000'
 ];
 
 function cors(req, res, next) {
